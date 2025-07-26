798ec78ab37e6fb6ca91eb7d30b8501935b9612e
Rayan <154925122+RayaneGuebre@users.noreply.github.com>
2025-07-26 19:44:17 +0000

    fixed minor issues

--- code.ino
+++ code.ino
@@ -15,14 +15,14 @@ Servo myServo;
 #define x_limit_pin_end 3
 #define y_limit_pin_end 4
 #define servo_pin 5
+#define INTERFACE_TYPE 1
 
-AccelStepper x_stepper(INTERFACE_TYPE, y_stepper_pin, x_stepper_dir_pin);
+AccelStepper x_stepper(INTERFACE_TYPE, x_stepper_pin, x_stepper_dir_pin);
 AccelStepper y_stepper(INTERFACE_TYPE, y_stepper_pin, y_stepper_dir_pin);
 
-bool resetted_x_axis == false
-x_len_step = 0;
-y_len_step = 0;
-setted_up = false;
+long x_len_step = 0;
+long y_len_step = 0;
+bool setted_up = false;
 
 void setup() {
 
@@ -32,11 +32,16 @@ pinMode(x_stepper_dir_pin, OUTPUT);
 pinMode(y_stepper_dir_pin, OUTPUT);
 
 
-pinMode(x_limit_pin, INPUT_PULLUP);
-pinMode(y_limit_pin, INPUT_PULLUP);
+pinMode(x_limit_pin_start, INPUT_PULLUP);
+pinMode(y_limit_pin_start, INPUT_PULLUP);
+pinMode(x_limit_pin_end, INPUT_PULLUP);
+pinMode(y_limit_pin_end, INPUT_PULLUP);
 
 x_stepper.setMaxSpeed(1000);      
 x_stepper.setAcceleration(500);
+
+y_stepper.setMaxSpeed(1000);      
+y_stepper.setAcceleration(500);
 
 myServo.attach(servo_pin);
     
@@ -48,7 +53,7 @@ void loop() {
 
     // x axis setup
     x_stepper.moveTo(-100000);
-    while (x_limit_pin_start == HIGH){
+    while (digitalRead(x_limit_pin_start) == HIGH){
         x_stepper.run();
     }
 
@@ -57,26 +62,30 @@ void loop() {
 
     // y axis setup
     y_stepper.moveTO(-10000);
-    while (y_limit_pin_start == HIGH){
-        x_stepper.run();
+    while (digitalRead(y_limit_pin_start) == HIGH){
+        y_stepper.run();
     }
     y_stepper.stop(); 
     y_stepper.setCurrentPosition(0);
 
 
+    
+
      // board Measuring
 
-    x_stepper.moveTO(100000)
-    while (x_limit_pin_end == HIGH){
+    x_stepper.moveTo(100000);
+    while (digitalRead(x_limit_pin_end) == HIGH){
     x_stepper.run();
     }
-    x_len_step = x_stepper.currentPosition()
-
+    x_len_step = x_stepper.currentPosition();
+ 
      
-    y_stepper.moveTO(100000)
-    while (y_limit_pin_end == HIGH){
+    y_stepper.moveTo(100000);
+    while (digitalRead(y_limit_pin_end) == HIGH){
     y_stepper.run();
     }
-    y_len_step = y_stepper.currentPosition()
-}
+    y_len_step = y_stepper.currentPosition();
+
+    setted_up = true;
+    }
 }
